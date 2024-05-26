290cbecd283f170e6c3a5c912515eb66d7857d3d
cryptomathematician <bosman006@st.ug.edu.gh>
2024-05-26 04:39:28 +0000

    Task 4(Made the name text bold)

--- my-app/App.js
+++ my-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Bayat Osman</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>Bayat Osman</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +19,9 @@ const styles = StyleSheet.create({
   }, 
   text: {
     fontSize: 24,
+  },
+  boldText: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
